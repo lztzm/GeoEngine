00bf78f95ce33c433d01c7adfa7c098b57cf9dc9
lztzm <tsetimie@gmail.com>
2024-12-03 23:39:24 +0800

    Update 3_📍_Marker_Cluster.py

--- pages/3_📍_Marker_Cluster.py
+++ pages/3_📍_Marker_Cluster.py
@@ -18,7 +18,7 @@ st.title("Marker Cluster")
 with st.expander("See source code"):
     with st.echo():
 
-        m = leafmap.Map(center=[40, -100], zoom=4)
+        m = leafmap.Map(center=[24.14734503954353, 120.67316364818225], zoom=10)
         filepath = "https://raw.githubusercontent.com/lztzm/Geofile/main/%E5%9C%96%E6%AA%94/%E8%87%BA%E4%B8%AD%E5%B8%82%E8%B7%AF%E5%A4%96%E5%81%9C%E8%BB%8A%E5%A0%B4%E5%BA%A7%E6%A8%99%E8%B3%87%E6%96%99.csv"
 
         m.add_points_from_xy(
