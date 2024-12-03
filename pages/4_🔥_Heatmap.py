e9d310b199072c3ace070d1f6dcb2cc41eb6633d
lztzm <tsetimie@gmail.com>
2024-12-03 23:24:36 +0800

    Update 4_🔥_Heatmap.py

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -18,12 +18,12 @@ st.title("Heatmap")
 with st.expander("See source code"):
     with st.echo():
         filepath = "https://github.com/lztzm/Geofile/blob/main/圖檔/臺中市路外停車場座標資料.csv"
-        m = leafmap.Map(center=[24.148805, 120.671363], zoom=4)
+        m = leafmap.Map(center=[24.14734503954353, 120.67316364818225], zoom=)
         m.add_heatmap(
             filepath,
             latitude="緯度(Y座標)",
             longitude="經度(X座標)",
-            value="pop_max",
+            value="位址",
             name="Heat map",
             radius=20,
         )
