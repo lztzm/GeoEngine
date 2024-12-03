a3c6abc1c67a8cc72d4f428d8431f4ac18ee4657
lztzm <tsetimie@gmail.com>
2024-12-03 23:17:50 +0800

    Update 4_🔥_Heatmap.py

--- pages/4_🔥_Heatmap.py
+++ pages/4_🔥_Heatmap.py
@@ -17,12 +17,12 @@ st.title("Heatmap")
 
 with st.expander("See source code"):
     with st.echo():
-        filepath = "https://raw.githubusercontent.com/giswqs/leafmap/master/examples/data/us_cities.csv"
-        m = leafmap.Map(center=[40, -100], zoom=4)
+        filepath = "https://github.com/lztzm/Geofile/blob/main/%E5%9C%96%E6%AA%94/%E8%87%BA%E4%B8%AD%E5%B8%82%E8%B7%AF%E5%A4%96%E5%81%9C%E8%BB%8A%E5%A0%B4%E5%BA%A7%E6%A8%99%E8%B3%87%E6%96%99.csv"
+        m = leafmap.Map(center=[24.148805, 120.671363], zoom=4)
         m.add_heatmap(
             filepath,
-            latitude="latitude",
-            longitude="longitude",
+            latitude="緯度(Y座標)",
+            longitude="經度(X座標)",
             value="pop_max",
             name="Heat map",
             radius=20,
